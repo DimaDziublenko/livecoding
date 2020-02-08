45926b603e888075bbb1c98a34f25a8ea18e9f8d
DimaDziublenko <56675165+DimaDziublenko@users.noreply.github.com>
2020-02-08 11:40:21 +0200

    feature: initial commit

---
+++ src/test/java/com/ite/livecoding/LivecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ite.livecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
